9f93250471747280b2dccb2193f2b95cee7707fd
gness1804 <graham.nessler@gmail.com>
2022-05-12 16:52:30 -0500

    all tests passing

--- collatz-conjecture.ts
+++ collatz-conjecture.ts
@@ -1,3 +1,20 @@
-export function steps(count: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function steps(count: number): number {
+  let numberOfSteps = 0;
+  let leftoverNumber = count;
+
+  if (count < 0) throw new Error('Only positive numbers are allowed');
+  if (count === 0) throw new Error('Only positive numbers are allowed');
+  if (count === 1) return 0;
+
+  while (leftoverNumber !== 1) {
+    numberOfSteps++;
+    const isEven = leftoverNumber % 2 == 0;
+    if (isEven) {
+      leftoverNumber = leftoverNumber / 2;
+    } else {
+      leftoverNumber = (3 * leftoverNumber) + 1;
+    }
+  }
+
+  return numberOfSteps;
 }
